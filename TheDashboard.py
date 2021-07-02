688b5fc8aa5edd2a556e188689d499cc4ce00284
hoteitjaafar <34979075+hoteitjaafar@users.noreply.github.com>
2021-07-02 14:48:49 +0300

    Update TheDashboard.py

--- TheDashboard.py
+++ TheDashboard.py
@@ -4,7 +4,7 @@ st.write('''Thank you for accessing the link. Please use the navigation tool to 
               Thank you,
               Jaafar Hoteit.'''
 
-select_box = st.selectbox('Navigate Here', ['PowerBi Dashboard']) 
+select_box = st.selectbox('Navigate Here', ('PowerBi Dashboard')) 
 
 if select_box == 'PowerBi Dashboard':
   st.markdown('https://app.powerbi.com/reportEmbed?reportId=8e8d2831-e6a5-4737-832f-ec1ebb4154fa&autoAuth=true&ctid=c7ba5b1a-41b6-43e9-a120-6ff654ada137&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9', unsafe_allow_html=True)
