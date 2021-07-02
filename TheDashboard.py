6fe692cdb2426ad29e526f74d1fc9ca765d21c54
hoteitjaafar <34979075+hoteitjaafar@users.noreply.github.com>
2021-07-02 15:21:10 +0300

    Update TheDashboard.py

--- TheDashboard.py
+++ TheDashboard.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('Hello')
 st.write("Please press on the below link to view Jaafar Hoteit's PowerBi Dashboard")
-st.markdown('https://app.powerbi.com/reportEmbed?reportId=8e8d2831-e6a5-4737-832f-ec1ebb4154fa&autoAuth=true&ctid=c7ba5b1a-41b6-43e9-a120-6ff654ada137&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9', allow_unsafe_html=True)
+st.markdown('https://app.powerbi.com/reportEmbed?reportId=8e8d2831-e6a5-4737-832f-ec1ebb4154fa&autoAuth=true&ctid=c7ba5b1a-41b6-43e9-a120-6ff654ada137&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9', unsafe_allow_html=True)
