f02581c2595ddf438b4aff78e5d0d1107e7fb6bc
hoteitjaafar <34979075+hoteitjaafar@users.noreply.github.com>
2021-07-02 14:54:09 +0300

    Update TheDashboard.py

--- TheDashboard.py
+++ TheDashboard.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
-#st.write('''Thank you for accessing the link. Please use the navigation tool to access the Desired Dashboard. 
- #             Thank you,
- #             Jaafar Hoteit.''')
+st.write('''Thank you for accessing the link. Please use the navigation tool to access the Desired Dashboard. 
+              Thank you,
+              Jaafar Hoteit.''')
 
-#option = st.selectbox('Navigate Here', ['PowerBi Dashboard']) 
+option = st.selectbox('Navigate Here', ['PowerBi Dashboard']) 
 
-#if option == 'PowerBi Dashboard':
-  #st.markdown('https://app.powerbi.com/reportEmbed?reportId=8e8d2831-e6a5-4737-832f-ec1ebb4154fa&autoAuth=true&ctid=c7ba5b1a-41b6-43e9-a120-6ff654ada137&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9', unsafe_allow_html=True)
-st.markdown('<iframe width="900" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=8e8d2831-e6a5-4737-832f-ec1ebb4154fa&autoAuth=true&ctid=c7ba5b1a-41b6-43e9-a120-6ff654ada137&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)       
+if option == 'PowerBi Dashboard':
+  st.markdown('https://app.powerbi.com/reportEmbed?reportId=8e8d2831-e6a5-4737-832f-ec1ebb4154fa&autoAuth=true&ctid=c7ba5b1a-41b6-43e9-a120-6ff654ada137&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9', unsafe_allow_html=True)
